6c4d9b1c24e699d1db96c1546d7e1151da6f472b
Ritesh kharal <kharalritesh@gmail.com>
2026-02-18 19:01:56 +0545

    Everything Set Up !

---
+++ app/components/ThemeToggle.jsx
@@ -0,0 +1,26 @@
+// Temporary button, add color choosings later on
+
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export function ThemeToggle() {
+  const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // eslint-disable-next-line react-hooks/set-state-in-effect
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  return (
+    <button
+      onClick={() =>
+        setTheme(resolvedTheme === "dark" ? "light" : "dark")
+      }
+      className="p-2 rounded bg-surface transition-colors"
+    >
+      {resolvedTheme === "dark" ? "🌞" : "🌙"}
+    </button>
+  );
+}
