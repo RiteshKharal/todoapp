c65a014a619c8f0bd0e33dbdf3e880e8b04b75fe
Ritesh kharal <kharalritesh@gmail.com>
2026-02-22 19:49:10 +0545

    Login, signup DONE! Login/signUp based functionality TO BE.

--- app/components/SignUp.tsx
+++ app/components/SignUp.tsx
@@ -3,6 +3,8 @@
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import bcrypt from 'bcrypt'
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
@@ -10,22 +12,25 @@ const adapter = new PrismaPg({
 
 const prisma = new PrismaClient({adapter});
 
+let user;
+
 type acctypes = {
   email:string,
   password: string
 }
 
-let user;
 
 export async function acc(formdata: FormData){
 
   const Email = formdata.get('email')?.toString() || ''
   const pass = formdata.get('password')?.toString() || ''
+  const name = formdata.get('name')?.toString() || ''
 
   const PHash = await bcrypt.hash(pass,10)
 
     await prisma.user.create({
       data:{
+        name:name,
         email: Email,
         password: PHash
       }
@@ -35,17 +40,39 @@ export async function acc(formdata: FormData){
 
 }
 
-export async function logacc(formdata: FormData){
+ export async function logacc(formdata: FormData){
     const Email = formdata.get('email')?.toString() || ''
   const pass = formdata.get('password')?.toString() || ''
 
-  const PHash = await bcrypt.hash(pass,10)
 
-    user = await  prisma.user.findFirst({
+    user = await  prisma.user.findUnique({
       where:{
         email: Email,
-        password: PHash
       }
     })
+
+    if (!user) return;
+
+  const PassMatch =await bcrypt.compare(pass,user.password)
+
+  if(!PassMatch) return;
+
+  (await cookies()).set('UID', user.id)
+
+  // return user;
+  
 }
+
+export async function getUser(){
+  const usid= (await cookies()).get('UID')?.value
+
+  if (!usid) return;
+
+  return prisma.user.findUnique({
+    where:{
+      id:usid
+    }
+  })
+}
+
 
--- app/components/accmanager.tsx
+++ app/components/accmanager.tsx
@@ -1,15 +1,38 @@
 'use client';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import * as fonts from '@/app/font/fonts'
-import {acc} from './SignUp'
+import {acc,logacc, getUser} from './SignUp'
+
 
 type AccManagerProps = {
   cardtype: string
 }
 
+type UserType = {
+  id: string
+  email: string
+  name: string
+  password: string
+}
 
 export default function Accmanager({ cardtype }: AccManagerProps) {
+  
+
+const [user, setUser] = useState<UserType | null>(null)
   const [lstype, setLstype] = useState<string | null>(null)
+
+  
+    async function fetchUser() {
+          const u = await getUser()
+          if (u) setUser(u ?? null)
+        }
+
+  useEffect(() => {
+    
+    fetchUser()
+  }, [])
+
+
   
         function Login({ close }: { close: () => void }) {
             return (
@@ -26,6 +49,7 @@ export default function Accmanager({ cardtype }: AccManagerProps) {
                     Log onto Account
                 </h2>
 
+              <form action={logacc}>
                 <div className="flex flex-col gap-5 flex-1">
                     <input
                     type="email"
@@ -40,18 +64,19 @@ export default function Accmanager({ cardtype }: AccManagerProps) {
                     />
                 </div>
 
-                <button className="mt-8 bg-primary/30 hover:bg-primary/40 transition p-3 rounded-xl font-medium cursor-pointer">
+                <button className="mt-8 bg-primary/30 hover:bg-primary/40 transition p-3 rounded-xl font-medium cursor-pointer" type='submit'>
                     Log in
                 </button>
+                </form>
 
                 <small className='mt-5 '><button onClick={()=>{setLstype('SignUp')}} className='cursor-pointer'>Already got a account? Click here to login</button></small>
 
                 </div>
             
             )
-            }
+        }
 
-            function Signup({ close }: { close: () => void }) {
+        function Signup({ close }: { close: () => void }) {
             return (
                 <div className="relative w-full max-w-md bg-primary rounded-2xl p-8 flex flex-col">
 
@@ -69,11 +94,20 @@ export default function Accmanager({ cardtype }: AccManagerProps) {
                 <form action={acc}>
 
                 <div className="flex flex-col gap-5 flex-1">
+                  <input
+                    type="text"
+                    placeholder="Username"
+                    name='name'
+                    className="w-full p-3 rounded-lg bg-primary/20 outline-none focus:ring-2 focus:ring-primary/40"
+                    required
+                    />
+
                     <input
                     type="email"
                     placeholder="Email"
                     className="w-full p-3 rounded-lg bg-primary/20 outline-none focus:ring-2 focus:ring-primary/40"
                     name='email'
+                    required
                     />
 
                     <input
@@ -81,28 +115,49 @@ export default function Accmanager({ cardtype }: AccManagerProps) {
                     placeholder="Password"
                     name='password'
                     className="w-full p-3 rounded-lg bg-primary/20 outline-none focus:ring-2 focus:ring-primary/40"
+                    required
                     />
                 </div>
 
-                <input type='submit' className="mt-8 bg-primary/30 hover:bg-primary/40 transition p-3 rounded-xl font-medium cursor-pointer" placeholder='Sign Up'>
-                    
-                </input>
+                <button type='submit' className="mt-8 bg-primary/30 hover:bg-primary/40 transition p-3 rounded-xl font-medium cursor-pointer" >
+                    Sign Up
+                </button>
 
               </form>
                 <small><button onClick={()=>{setLstype('Login')}} className='mt-5 cursor-pointer'>Already got a account? Click here to login</button></small>
 
                 </div>
             )
-            }
+        }
+        
+        function options(){
+          return user ? (
+    <div
+      className={`bg-primary p-2 text-sm rounded-xl ${fonts.cabin.className} cursor-pointer`}
+    >
+      {user.name}
+    </div>
+  ) : (
+    <div
+      onClick={() => setLstype('Signup')}
+      className={`bg-primary p-2 text-sm rounded-xl ${fonts.cabin.className} cursor-pointer`}
+    >
+      Sign Up
+    </div>
+  );
+          
+
+        }
+
+        // useEffect(options,[user])
+
 
   return (
     <>
-      <div
-        onClick={() => setLstype('Signup')}
-        className={`bg-primary p-2 text-sm rounded-xl ${fonts.cabin.className} cursor-pointer`}
-      >
-        Sign Up
-      </div>
+
+    
+
+      {options()}
 
       {lstype && (
         <div className="fixed inset-0 z-50 flex justify-center items-center backdrop-blur-sm">
@@ -115,6 +170,3 @@ export default function Accmanager({ cardtype }: AccManagerProps) {
     </>
   )
 }
-
-
-
